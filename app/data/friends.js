2ca2a5cda81845bf80ec5e1260d6bbafc38fc7b5
Benitez <benitez.marcos1@gmail.com>
2019-05-22 22:33:03 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [ {
+    "name" : "Chris",
+    "photo" : "https://imgur.com/GxDcXB5",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "May",
+    "photo" : "https://imgur.com/lsmCrhb",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "John",
+    "photo" : "https://imgur.com/EuAL142",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Amy",
+    "photo" : "https://imgur.com/MLdtxUk",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Tristan",
+    "photo" : "https://imgur.com/T0AZaUg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Kristi",
+    "photo" : "https://imgur.com/W5dAal8",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}];
+
+module.exports = friends;
